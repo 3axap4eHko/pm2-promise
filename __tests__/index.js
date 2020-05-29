23399fb4cc7a3ec3cf3bef4cf897cb71980c29e8
Ivan Zakharchanka <3axap4eHko@gmail.com>
2020-05-28 21:47:50 -0400

    pm2 upgrade

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,23 +1,24 @@
 const pm2 = require('../index');
 const API = pm2.custom;
 
-test('Test promise', async () => {
-  const methods = Object.keys(API.prototype);
-
-  expect(methods).not.toHaveLength(0);
-
-  Object.keys(API.prototype).forEach(name => {
-    expect(typeof API.prototype[name]).toBe('function');
+describe('PM2 promise test suite', () => {
+  it('Should have non-zero methods', () => {
+    const methods = Object.keys(API.prototype);
+    expect(methods.length).toBeGreaterThan(0);
   });
 
-  Object.keys(API.prototype).forEach(name => {
-    expect(typeof pm2[name]).toBe('function');
+  it('Should have the same methods as original PM2', () => {
+    Object.keys(API.prototype).forEach(name => {
+      expect(typeof pm2[name]).toBe('function');
+    });
   });
 
-  await pm2.connect();
-  const bus = await pm2.launchBus();
-  expect(bus).toBeDefined();
-
-  await pm2.close();
+  it('Should be able to connect to daemon', async () => {
+    await pm2.connect();
+    const bus = await pm2.launchBus();
+    expect(bus).toBeDefined();
+    await pm2.close();
+  });
 });
+
 
